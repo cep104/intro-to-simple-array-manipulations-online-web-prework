2800a0f7485d5d5da11232ae0c5d7a716473aaf3
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-18 17:19:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,5 +25,5 @@ end
 def using_concat(@my_favorite_things, more_favs)
 @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["mario kart", "flatiron school"]
-    @all_my_favs = using_concat(@my_favorite_things, more_favs)
+    
 end
