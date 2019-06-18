206cd90b4c4ff1ed59eeaa3a738017bf2fb98b20
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-18 17:45:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -44,7 +44,7 @@ def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten
 end
 
-def using_delete(instructors, "Steven")
+def using_delete(instructors,"Steven")
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   instructors.delete("Steven")
 end
